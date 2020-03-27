9fd03fc31745697281bd04e163174a37c4193e26
Tri S <mas.trisutrisno@gmail.com>
2020-03-27 18:13:14 +0700

    Create Footer.vue

---
+++ src/views/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-footer class="subtitle-1" color="#f1f1f1" height="auto" app>
+    <div class="grey--text subtitle-2 ml-auto">
+      &copy;2020,
+      <strong>TaskForce</strong>
+    </div>
+  </v-footer>
+</template>
+<script>
+export default {};
+</script>
