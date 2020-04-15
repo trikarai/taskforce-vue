f61c5362e7733c23934df1ed902d8eef21507b01
Tri S <mas.trisutrisno@gmail.com>
2020-04-15 18:55:35 +0700

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -6,7 +6,8 @@ if (process.env.NODE_ENV === "production") {
   base.api = window.location.origin + "/bara-taskforce/public/api";
   base.storage = window.location.origin + "/bara-taskforce/storage/app";
 } else {
-  base.api = "http://localhost/bara-taskforce/public/api";
-  base.storage = "http://localhost/bara-taskforce/storage/app";
+  var localhost = "http://localhost";
+  base.api = localhost + "/bara-taskforce/public/api";
+  base.storage = localhost + "/bara-taskforce/storage/app";
 }
 export const baseUri = base;
